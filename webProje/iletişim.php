f8edabdd21028faf2acee4fe360f0d92874fb99e
hakanEfeTuysuz <hakanefetuysuz@gmail.com>
2024-05-19 21:31:11 +0300

    finish the project
    Projemi ufak tefek sorunlar olsa da bitirdim.

--- webProje/iletişim.php
+++ webProje/iletişim.php
@@ -1,17 +1,19 @@
 <?php
-// Form verilerini POST yöntemiyle alalım
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$gender = $_POST['gender'];
-$topic = $_POST['topic'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Form verilerini POST yöntemiyle alalım
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $gender = $_POST['gender'];
+    $topic = $_POST['topic'];
 
-// Verileri ekrana yazdırabiliriz
-echo "Ad: " . $name . "<br>";
-echo "E-posta: " . $email . "<br>";
-echo "Telefon: " . $phone . "<br>";
-echo "Mesaj: " . $message . "<br>";
-echo "Cinsiyet: " . $gender . "<br>";
-echo "Konu: " . $topic . "<br>";
+    // Verileri ekrana yazdırabiliriz
+    echo "Ad: " . htmlspecialchars($name) . "<br>";
+    echo "E-posta: " . htmlspecialchars($email) . "<br>";
+    echo "Telefon: " . htmlspecialchars($phone) . "<br>";
+    echo "Mesaj: " . htmlspecialchars($message) . "<br>";
+    echo "Cinsiyet: " . htmlspecialchars($gender) . "<br>";
+    echo "Konu: " . htmlspecialchars($topic) . "<br>";
+}
 ?>
